c38ce0ac96c0caa463d3264ff75d12a1bb16ae46
jhonnatan.uruena <jhonnatan.uruena@sinco.co>
2025-10-31 09:24:23 -0500

    Add activity determination logic for 7 AM in MorningRoutine.

--- MorningRoutine/Routine.cs
+++ MorningRoutine/Routine.cs
@@ -5,10 +5,12 @@ public class Routine(IClock clock)
     public string WhatShould()
     {
         var hour = clock.Now.Hour;
-        if (hour == 6)
-        {
+        if (hour == 6)  
             return "Hacer ejercicio";
-        }
+
+
+        if (hour == 7)
+            return "Leer y estudiar";
 
         return "Sin Actividad";
     }
