5c6ad86df70227d939afdc17ada723355573a77a
jhonnatan.uruena <jhonnatan.uruena@sinco.co>
2025-10-31 11:49:30 -0500

    Convert `Activity` class to a record for improved immutability and concise property management

--- MorningRoutine/Activity.cs
+++ MorningRoutine/Activity.cs
@@ -1,11 +1,7 @@
 namespace MorningRoutine;
 
-public class Activity(TimeSpan start, TimeSpan end, string description)
+public record Activity(TimeSpan Start, TimeSpan End, string Description)
 {
-    public string Description { get; } = description;
-    public TimeSpan End { get; } = end;
-    public TimeSpan Start { get; } = start;
-
     public bool ContainsTime(TimeSpan time)
     {
         return time >= Start && time <= End;
