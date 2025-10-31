e51d9d650646b35d6826826a415155b103573979
jhonnatan.uruena <jhonnatan.uruena@sinco.co>
2025-10-31 09:28:02 -0500

    Refactor `WhatShould` method to extract hour retrieval logic into `GetNowHour` for clarity and reusability.

--- MorningRoutine/Routine.cs
+++ MorningRoutine/Routine.cs
@@ -2,9 +2,11 @@ namespace MorningRoutine;
 
 public class Routine(IClock clock)
 {
+
+    
     public string WhatShould()
     {
-        var hour = clock.Now.Hour;
+        var hour = GetNowHour();
         return hour switch
         {
             6 => "Hacer ejercicio",
@@ -12,5 +14,10 @@ public class Routine(IClock clock)
             8 => "Desayunar",
             _ => "Sin Actividad"
         };
+    }
+
+    private int GetNowHour()
+    {
+        return clock.Now.Hour;
     }
 }
