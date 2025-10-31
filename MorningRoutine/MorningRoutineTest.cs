674279d429c9aecda735e62d49a22c8e8d275571
jhonnatan.uruena <jhonnatan.uruena@sinco.co>
2025-10-31 09:23:42 -0500

    Add test for activity determination at 7 AM in MorningRoutineTest.

--- MorningRoutine/MorningRoutineTest.cs
+++ MorningRoutine/MorningRoutineTest.cs
@@ -16,5 +16,17 @@ public class MorningRoutineTest
         resultado.Should().Be("Hacer ejercicio");
     }
 
-  
+    [Fact]
+    public void SiLaHoraEs7_Debe_MostrarLeerEstudiar()
+    {
+        //Arrange
+        var clock = new FakeClock(new DateTime(2025, 1, 1, 7, 0, 0));
+        var routine = new Routine(clock);
+
+        //Act
+        var resultado = routine.WhatShould();
+
+        //Assert
+        resultado.Should().Be("Leer y estudiar");
+    }
 }
